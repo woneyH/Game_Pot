9bd80d04fbba0585e9274eab4908254fc0f9b829
김원혁 <kwh102455@gmail.com>
2025-09-18 23:09:51 +0900

    ✨ feat : springboot project commit

---
+++ backend/src/main/java/pbl_game_pot/game_pot/GamePotApplication.java
@@ -0,0 +1,13 @@
+package pbl_game_pot.game_pot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pbl_game_pot/game_pot/GamePotApplicationTests.java
@@ -0,0 +1,13 @@
+package pbl_game_pot.game_pot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
